6509b4811982f52a697ef05205b86487065b1740
itsAPK <apkvandagadde2@gmail.com>
2024-12-27 03:36:51 +0530

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("max-w-[100%] overflow-x-hidden")}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <TRPCReactProvider>{children}</TRPCReactProvider>{" "}
 
         </ThemeProvider>
